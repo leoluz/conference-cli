e8cba1c3f4bb1e435fdd4ac4d7a2f9a56b00bc22
Leonardo Luz Almeida <leonardo.la@gmail.com>
2016-03-10 22:04:28 -0500

    externalize the token info

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
-var cli = require('commander')
-var restify = require('restify')
-var assert = require('assert')
+var cli = require('commander');
+var restify = require('restify');
+var assert = require('assert');
+var fs = require('fs');
+
+var token = fs.readFileSync('token', 'utf8').toString().split('\n')[0];
+
+if (!token) {
+    console.log('Error: token file not found!');
+    process.exit(1);
+}
 
 var client = restify.createJsonClient({
     url: 'http://localhost:3000',
@@ -48,11 +56,11 @@ function printTalkInfo(talkId) {
 }
 
 function buildOptions(endpoint) {
-        return {
-          path: endpoint,
-          headers: {
-            'Authorization': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjBlNmQ2MWFmNzdjMjI3NzIyZjdmODI2MjBmYzZhY2NlZGEwM2UzODEifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXRfaGFzaCI6IkZTOW1KNFliX1RSTGhSOUVmLTN2S0EiLCJhdWQiOiIxNDcwMjIyNTM2MDgtcDNuYmxlbmZkczFxNGhxbmE0bjdsYzRsYzRtOWFvaTYuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTA1NzY4NzY5NTk5NDUwOTIxNTIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXpwIjoiMTQ3MDIyMjUzNjA4LXAzbmJsZW5mZHMxcTRocW5hNG43bGM0bGM0bTlhb2k2LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiZW1haWwiOiJsZW9uYXJkby5sYUBnbWFpbC5jb20iLCJpYXQiOjE0NTc2MzY1ODEsImV4cCI6MTQ1NzY0MDE4MSwibmFtZSI6Ikxlb25hcmRvIEx1eiIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vLXU1Xy1Xa0luVGxBL0FBQUFBQUFBQUFJL0FBQUFBQUFBRkE4L1ZzcFcyT0xGbUJnL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJMZW9uYXJkbyIsImZhbWlseV9uYW1lIjoiTHV6IiwibG9jYWxlIjoiZW4ifQ.JR4yJNlx_dR-BWAcQKJxJBnB-8qO3dpL2rs5m1Khge2MZjnpYYbzKwl-4MVpY52v35Bdk2EmGske-eWGthv1ioiARzJbCVszYowVfjPr6dc_y4lMbcKXnFvJzvFoPYnuOcpqgcbrXNzq62lPhtAPLSd45lHU1KvZKgaP7nwGfQzEPnaZ8VJvl3MOYHfZSJJ2mPTwYXqOoCjpbAMXxJlu1Hna-Rb0JbWgje2kQgeFbj6KCA8Gci91KgNn0EbWlwUqaYRlNpuqoBANiIe-xwixfR4Jawrnpr_eTkD7bgu_Z63Fm2hLJ8mfnAE8ywFXNPnt6XIDUL4LkKp9M9T0pdu3FQ',
-            'hybris-tenant': 'conference'
-          }
-        };
+    return {
+      path: endpoint,
+      headers: {
+        'Authorization': token,
+        'hybris-tenant': 'conference'
+      }
+    };
 }
